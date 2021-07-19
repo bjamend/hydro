fdef0f76ab2e72088e97019c0d02192069bbbb36
Benjamin Amend <bamend@clemson.edu>
2021-07-19 13:58:20 -0400

    Demonstrate cusps indicating a bug

--- hydro.c
+++ hydro.c
@@ -250,21 +250,30 @@ void initialize_primitive(double *primitive, double dx, int n, double x0) {
   for (int i = 0; i < n; ++i) {
     double x = x0 + (i + 0.5) * dx;
     double *prim = &primitive[3*i];
-    prim[0] = gaussian(x);
-    prim[1] = 1;
-    prim[2] = 1;
+    if (x < 0.5) {
+      prim[0] = 1;
+      prim[1] = 5;
+      prim[2] = 1;
+    } else {
+      prim[0] = 1;
+      prim[1] = -5;
+      prim[2] = 1;
+    }
   }
 }
 
 int main() {
-  const double tmax  = 0.5;
+  const double tmax  = 0.06;
   const int n        = 1000;
   const double xl    = 0;
   const double xr    = 1;
   const double dx    = (xr - xl) / n;
-  const double chkpt = 0.0025;
-  double plm_theta   = 1.5;
-  //double cfl_number  = 0.4;
+  const double chkpt = 0.001;
+  const double dt    = 0.0001;
+  double t           = 0;
+  double plm_theta   = 1.3;
+  int j              = 0;
+  double s[2];
 
   double primitive[3*n];
   double conserved[3*n];
@@ -290,15 +299,8 @@ int main() {
     conserved2[3*n-i-1] = conserved[3*n-i-1];
   }
 
-  double t = 0;
-  int j = 0;
-  double dt = 0.0001;
-  double s[2];
-  //double a1 = 0;
-
   // Evolve the simulation in time.
   while (t < tmax) {
-    //double a = 0;
     // Update the simulation in space.
     for (int i = 2; i < (n-2); ++i) {
       double *cons_im2 = &conserved[3*(i-2)];
@@ -314,7 +316,9 @@ int main() {
 			conserved1[3*i+0] = conserved[3*i+0] - (f_iph[0] - f_imh[0]) * dt / dx;
       conserved1[3*i+1] = conserved[3*i+1] - (f_iph[1] - f_imh[1]) * dt / dx;
       conserved1[3*i+2] = conserved[3*i+2] - (f_iph[2] - f_imh[2]) * dt / dx;
+    }
 
+    for (int i = 2; i < (n-2); ++i) {
       double *cons_im2_1 = &conserved1[3*(i-2)];
       double *cons_im1_1 = &conserved1[3*(i-1)];
       double *cons_i00_1 = &conserved1[3*(i+0)];
@@ -331,7 +335,9 @@ int main() {
                           (f_iph1[1] - f_imh1[1]) * dt / dx / 4;
       conserved2[3*i+2] = 3 * conserved[3*i+2] / 4 + conserved1[3*i+2] / 4 -
                           (f_iph1[2] - f_imh1[2]) * dt / dx / 4;
+    }
 
+    for (int i = 2; i < (n-2); ++i) {
       double *cons_im2_2 = &conserved2[3*(i-2)];
       double *cons_im1_2 = &conserved2[3*(i-1)];
       double *cons_i00_2 = &conserved2[3*(i+0)];
@@ -348,9 +354,6 @@ int main() {
                           2 * (f_iph2[1] - f_imh2[1]) * dt / dx / 3;
       conserved3[3*i+2] = conserved[3*i+2] / 3 + 2 * conserved2[3*i+2] / 3 -
                           2 * (f_iph2[2] - f_imh2[2]) * dt / dx / 3;
-
-      //a1 = max2(abs(s[0]), abs(s[1]));
-      //a = max2(a1, abs(a));
     }
 
     // Save conserved vectors to text files in checkpoint intervals.
@@ -372,8 +375,6 @@ int main() {
     for (int i = 6; i < (3*(n-2)); ++i) {
       conserved[i] = conserved3[i];
     }
-
-    //double dt = cfl_number * dx / a;
 
     t += dt;
   }
